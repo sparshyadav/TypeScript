0a09b81b8e423cf37fff9e3a3c82aba51459594e
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-01-19 09:00:04 +0530

    [Sparsh] Add: Practice Problems on TypeScript Basics

---
+++ PracticeProblems/Basics/LetConst.ts
@@ -0,0 +1,10 @@
+// Declare variables using let and const to store the following:
+// Your name (string)
+// Your age (number)
+// Whether you like TypeScript (boolean)
+
+const name1: string = "Sparsh";
+let age1: number = 22;
+let likeTypeScript: boolean = true;
+
+console.log(`My Name is: ${name1}, and I am ${age1} years old, and Do I Like TypeScript: ${likeTypeScript}`);
