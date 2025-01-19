724cdd46a95d75c8ec53684153408fd16e9de4a1
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-01-19 08:59:36 +0530

    [Sparsh] Add: Practice Problems on Arrays

---
+++ PracticeProblems/Arrays/FavLanguage.ts
@@ -0,0 +1,4 @@
+//Create a readonly array of your favorite programming languages and try to modify it. What error do you get?
+
+let favouriteLanguage: readonly string[] = ["C++", "JavaScript", "TypeScript"];
+console.log(favouriteLanguage);
---
+++ PracticeProblems/Arrays/OptionalTuple.ts
@@ -0,0 +1,4 @@
+// Create a tuple where the second element (number) is optional. Show how to handle it safely.
+
+let tup: [string, number?] = ["Sparsh"];
+console.log(tup);
