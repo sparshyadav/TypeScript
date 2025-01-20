d99396384ea55b98cef9e84048bccd2faa6765d5
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-01-20 06:42:16 +0530

    [Sparsh] Add: String Enums in TypeScript

---
+++ Enums/StringEnums.ts
@@ -0,0 +1,12 @@
+// String Enums
+enum Dir{
+    Up="UP",
+    Down="Down",
+    Left="Left",
+    Right="Right"
+}
+
+console.log(Dir.Up);
+console.log(Dir.Down);
+console.log(Dir.Left);
+console.log(Dir.Right);
