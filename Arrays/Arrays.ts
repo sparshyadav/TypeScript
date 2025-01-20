4478b1a4a8ef391d2aa638d94db50e947ed5bd1b
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-01-20 06:39:10 +0530

    [Sparsh] Add: Readonly, Tuples, and Array Sorting

---
+++ Arrays/Arrays.ts
@@ -0,0 +1,18 @@
+// Readonly Array
+let readArray: readonly number[] = [1, 2, 3, 4];
+
+
+
+// Tuple Array
+let tuple: [number, string, boolean] = [1, "Sparsh", true];
+
+
+
+// Sorting Arrays
+let arr = [3, 1, 6, 4, 7, 4, 8, 9, 1];
+
+arr.sort((a, b) => a - b); // Ascending Order
+console.log(arr);
+
+arr.sort((a, b) => b - a); // Descending Order
+console.log(arr);
