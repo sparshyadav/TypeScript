5ff702dd642cdf687ca96367519b735544848329
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-01-20 10:40:10 +0530

    [Sparsh] Delete: Deleting Unnecessary Files

--- PracticeProblems/Arrays/FavLanguage.ts
+++
@@ -1,4 +0,0 @@
-//Create a readonly array of your favorite programming languages and try to modify it. What error do you get?
-
-let favouriteLanguage: readonly string[] = ["C++", "JavaScript", "TypeScript"];
-console.log(favouriteLanguage);
--- PracticeProblems/Arrays/RectangleArea.ts
+++
@@ -1,8 +0,0 @@
-// Write a function that accepts a tuple of [number, number] representing the dimensions of a rectangle and returns its area.
-
-function area(arr: [number, number]): number {
-    return arr[0] * arr[1];
-}
-
-console.log(area([4, 5]));
-console.log(area([25, 12]));
