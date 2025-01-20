0fc6eedd91c85cd023a575972f86af58c5815997
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-01-20 06:29:42 +0530

    [Sparsh] Add: Enums Practice Problems

---
+++ Enums/BasicEnums.ts
@@ -0,0 +1,13 @@
+//Define both Numeric and String enums in TypeScript. Then, create a function that accepts either a numeric or a string enum value and logs a specific message based on the enum value. Implement the function and demonstrate its usage.
+// Explanation:
+// Create a numeric enum called StatusCode with values 200, 404, and 500.
+// Create a string enum called UserRole with values Admin, User, and Guest.
+enum StatusCode{
+    OK=200,
+    Error=404,
+    ServerError=500
+}
+
+enum UserRole{
+    
+}
