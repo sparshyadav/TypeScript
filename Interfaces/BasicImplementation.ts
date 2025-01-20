e05415c948190f2a104bb756d6eea92530af0978
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-01-20 06:47:00 +0530

    [Sparsh] Add: Interfaces Basic Implementation

---
+++ Interfaces/BasicImplementation.ts
@@ -0,0 +1,14 @@
+// Creating a Basic Interface
+interface User1 {
+    id: number;
+    name: string;
+    email: string;
+}
+
+const user1: User1 = {
+    id: 1,
+    name: "Sparsh",
+    email: "sparsh@gmail.com"
+}
+
+console.log(user1);
