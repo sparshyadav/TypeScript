ebdca422841bccfa5bcf46d389ce788375ace6b7
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-01-20 06:39:57 +0530

    [Sparsh] Add: Create Multidimentional Arrays

---
+++ Arrays/MultidimentionalArray.ts
@@ -0,0 +1,17 @@
+// Multidimentional Arrays
+let matrix: number[][] = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+let cube: number[][][] = [
+    [
+        [1, 2],
+        [3, 4]
+    ],
+    [
+        [5, 6],
+        [7, 8]
+    ]
+];
