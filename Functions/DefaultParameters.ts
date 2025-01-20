9806623cc4a3dd6d3d5eeeabfa31841cba9b97b4
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-01-20 06:44:23 +0530

    [Sparsh] Add: Default Paramteres in Functions

---
+++ Functions/DefaultParameters.ts
@@ -0,0 +1,7 @@
+// Default Parameters
+function calculateArea(length: number = 12, breadth: number = 10): number {
+    return length * breadth;
+}
+
+console.log(calculateArea(4, 6));
+console.log(calculateArea());
