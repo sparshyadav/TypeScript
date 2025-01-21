d6dddf082184613366d0b0537d0291d902fe0b82
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-01-21 10:13:07 +0530

    [Sparsh] Add: Medium Problems on Generics

---
+++ Generics/Medium/MergeObjects.ts
@@ -0,0 +1,9 @@
+//Write a generic function that merges properties from two objects into one. If a key exists in both objects, the second object’s value should override the first one.
+function mergeObj<T, U>(obj1: T, obj2: U): T&U{
+    return {...obj1, ...obj2};
+}
+
+const obj1 = { name: "Sparsh", age: 21 };
+const obj2 = { age: 22, city: "Delhi" };
+const merged = mergeObj(obj1, obj2);
+console.log(merged);
