8c0d1dbeb3726a7fa161dd50cf3ac36e73a66a2f
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-01-21 10:09:55 +0530

    [Sparsh] Add: Extending Generic Interfaces

---
+++ Generics/ExtendingGenInterface.ts
@@ -0,0 +1,12 @@
+interface Entity<ID> {
+    id: ID;
+  }
+  
+  interface Userr<ID> extends Entity<ID> {
+    name: string;
+  }
+  
+  // Example usage:
+  const user: Userr<number> = { id: 1, name: "Alice" };
+  console.log(user); // Output: { id: 1, name: "Alice" }
+  
